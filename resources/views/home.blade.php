61830dd795da9d8e6442f4ee8c9d7a535e60a357
vagrant <vagrant@homestead>
2021-04-05 13:52:31 +0000

    homebladeちょっとへんしゅう

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,45 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    {{-- プロフィール --}}
+    <div class="prof">
+        <div class="">
+            <img src="#">
+        </div>
+        <div class="">
+            <div class="">
+                <p>ユーザー名</p>
             </div>
+            <div>
+                <p>プロフィールプロフィールプロフィールプロフィールプロフィールプロフィールプロフィールプロフィールプロフィール</p>
+            </div>
+            <div>
+                <p>20</p>
+                <span>投稿</span>
+            </div>
+            <div>
+                <p>100</p>
+                <span>フォロー</span>
+            </div>
+        </div>
+        <div>
+            <div>
+                <span>あなたのフォロワー</span>
+            </div>
+            <div>
+                <div><img src="#"></div>
+                <div><img src="#"></div>
+                <div><img src="#"></div>
+                <div><img src="#"></div>
+            </div>
+            <div>
+                <a href="#">フォロワー一覧へ</a>
+            </div>
+        </div>
+    </div>
+    <div class="timeline">
+        <div class="title">
+            <span>新着作品</span>
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
+Route::get('/', 'HomeController@index');
 Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes();
